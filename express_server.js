631b9e7d428b2675509735dafbca0ea06892ca23
Harjas101 <anand.harjas@gmail.com>
2022-03-13 12:32:19 -0400

    added feature of only users can see their own url

--- express_server.js
+++ express_server.js
@@ -5,8 +5,6 @@ const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 const { set } = require("express/lib/application");
 
-
-
 const users = { 
   "userRandomID": {
     id: "userRandomID", 
@@ -20,9 +18,38 @@ const users = {
   }
 }
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: {
+        longURL: "https://www.tsn.ca",
+        userID: "aJ48lW"
+    },
+    i3BoGr: {
+        longURL: "https://www.google.ca",
+        userID: "aJ48lW"
+    },
+    ijz123: {
+      longURL: "https://www.hello.ca",
+      userID: "a3jscw"
+  }
 };
+const urlsForUser = function(id) {
+ // console.log('this is urlsforuser test id', id, urlDatabase)
+  const userUrls = {};
+  for (const shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === id) {
+      userUrls[shortURL] = urlDatabase[shortURL];
+    }
+  }
+  return userUrls;
+};
+const cookieMatch = function (cookie, userDatabase) {
+  for (const user in userDatabase) {
+    if (cookie === user) {
+      return true;
+    }
+  } return false;
+}
+
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -42,7 +69,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const currUser = users[currUserID]
   const templateVars = { 
     shortURL: req.params.shortURL, 
-    longURL: urlDatabase[shortURL],
+    longURL: urlDatabase[shortURL].longURL,
     user: currUser
     };
   res.render("urls_show", templateVars);
@@ -60,8 +87,7 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const currUserID = req.cookies["user_id"];
   const currUser = users[currUserID]
-  const templateVars = {urls: urlDatabase, user: currUser};
-  //console.log(username);
+  const templateVars = {urls: urlsForUser(currUserID), user: currUser};
   res.render("urls_index", templateVars);
 });
 app.get("/register", (req, res) => {
@@ -77,27 +103,55 @@ app.get("/login", (req, res) => {
   const templateVars = {urls: urlDatabase, user: currUser};
   res.render("urls_login", templateVars);
 });
+app.get("/u/:shortURL", (req, res) => {
+  if( urlDatabase[req.params.shortURL]){
+
+    return  res.redirect(urlDatabase[req.params.shortURL].longURL)
+  };
+res.status(404).send("site doesnt exist")
+});
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);// Log the POST request body to the console
-  const newURL = generateRandomString(6);
-  urlDatabase[newURL] = req.body.longURL;
-  console.log(urlDatabase)
-  res.redirect(`/urls/${newURL}`)
+  //console.log(req.body);// Log the POST request body to the console
+  //const newURL = generateRandomString(6);
+  const currUserID = req.cookies["user_id"]
+  if(currUserID){
+    const newURL = generateRandomString(6);
+    urlDatabase[newURL] = {longURL: req.body.longURL, userID: currUserID} ;
+    return res.redirect(`/urls/${newURL}`)
+  }
+  res.status(403).send("sorry you are not logged in")
+  //console.log(urlDatabase)
     //res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  console.log("req body" , req.body);// Log the POST request body to the console
-  console.log('req params', req.params)
+  const currUserID = req.cookies["user_id"];
   const { shortURL} = req.params
-  delete urlDatabase[shortURL]
+  const url = urlDatabase[shortURL]
+  if(!url){
+    return res.status(404).send('this url doesnt exist')
+  }
+  if(currUserID === url.userID ){
+   delete urlDatabase[shortURL]
+  } else {
+    return res.status(404).send("you are not authorized because you are not the owner of this url")
+  }
   res.redirect(`/urls`)
 })
 
 app.post("/urls/:shortURL/", (req, res) => {
   const { shortURL} = req.params
-  urlDatabase[shortURL] = req.body.longURL
+  const currUserID = req.cookies["user_id"];
+  const url = urlDatabase[shortURL]
+  if(!url){
+    return res.status(404).send('this url doesnt exist')
+  }
+  if(currUserID === url.userID ){
+    urlDatabase[shortURL].longURL = req.body.longURL
+  } else {
+    return res.status(404).send("you are not authorized because you are not the owner of this url")
+  }
   res.redirect(`/urls`)
 })
 
@@ -105,21 +159,20 @@ app.post("/login", (req, res) => {
   const userName = req.body.email;
   const password = req.body.password
   let userFound = null
-  console.log("this is pretest", users)
+  //console.log("this is pretest", users)
   for(userID in users ){
     let currUser = users[userID] 
     if(userName === currUser.email && password === currUser.password){
       userFound = currUser
-    } //else { 
-     // return res.status(403).send('this doesnt exist')
-    //}
-    console.log("this is my user", users[userID])
+      break
+    }  
+    
   }
     if(!userFound){
      return res.status(403).send('this doesnt exist')
     }
-  console.log("this is the username", userName);
-  console.log("this is the userfound", userFound)
+  console.log("this is the username", userName, userID);
+  //console.log("this is the userfound", userFound)
   res.cookie(`user_id`, userID);
   res.redirect(`/urls`);
   
@@ -155,7 +208,7 @@ app.post("/register", (req, res) => {
   }
   
   users[userID] = user
-  console.log('USERS', users)
+  //console.log('USERS', users)
   res.redirect(`/urls`)
 })
 
